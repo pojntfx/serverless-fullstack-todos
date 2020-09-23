467ae51308fe657b860f651f2ecccc60d0245a7a
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-09-23 20:47:59 +0200

    feat: Add GET actions to /todos and /todoId

--- pages/api/todos.js
+++ pages/api/todos.js
@@ -18,9 +18,24 @@ returning *;`;
       res.json({ id: newTodo.id, title: newTodo.title, body: newTodo.body });
 
       return;
+
+    case "GET":
+      const todos = await query`select * from todos;`;
+
+      res.statusCode = 200;
+      res.json(
+        todos.map((t) => ({
+          id: t.id,
+          title: t.title,
+          body: t.body,
+        }))
+      );
+
+      return;
+
     default:
       res.statusCode = 405;
-      res.end("method not allowd");
+      res.end("method not allowed");
 
       return;
   }
--- pages/api/todos/[todoId].js
+++ pages/api/todos/[todoId].js
@@ -1,10 +1,28 @@
+import query from "../../../lib/db";
+
 const todo = async (req, res) => {
   switch (req.method) {
     case "GET":
+      const [
+        todo,
+      ] = await query`select * from todos where id = ${req.query.todoId};`;
+
+      if (!todo) {
+        res.statusCode = 404;
+        res.end("todo not found");
+
+        return;
+      }
+
       res.statusCode = 200;
-      res.json({ id: 1, title: "asdf", body: "asdfasdf" });
+      res.json({
+        id: todo.id,
+        title: todo.title,
+        body: todo.body,
+      });
 
       return;
+
     default:
       res.statusCode = 405;
       res.end("method not allowed");

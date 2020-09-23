c556e5af99499012d399a89390b448210e549c9c
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-09-23 21:35:48 +0200

    feat: Add delete action to frontend

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 import fetcher from "../lib/fetcher";
 
 export const getServerSideProps = async ({ req }) => {
@@ -12,23 +12,40 @@ export const getServerSideProps = async ({ req }) => {
 };
 
 const Home = (props) => {
-  const { data, err } = useSWR("/api/todos", fetcher, {
+  const { data: todos, err } = useSWR("/api/todos", fetcher, {
     initialData: props.todos,
   });
 
   if (err) return <div>😞 Failed to load todos</div>;
-  if (!data) return <div>⌛ Loading todos ...</div>;
+  if (!todos) return <div>⌛ Loading todos ...</div>;
 
   return (
     <div>
       <h1>Todos</h1>
 
       <ul>
-        {data.map((todo, index) => (
+        {todos.map((todo, index) => (
           <li key={index}>
             <div>
               <h2>{todo.title}</h2>
               <p>{todo.body}</p>
+              <button
+                onClick={async () => {
+                  mutate(
+                    "/api/todos",
+                    todos.filter((oldTodo) => oldTodo.id != todo.id),
+                    false
+                  );
+
+                  await fetch(`/api/todos/${todo.id}`, {
+                    method: "DELETE",
+                  });
+
+                  mutate("/api/todos");
+                }}
+              >
+                Delete Todo
+              </button>
             </div>
           </li>
         ))}

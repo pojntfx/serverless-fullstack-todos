f49f18b0c5c63bf6c97f1320c1a09584dc85ebc8
Felix Pojtinger <felix@pojtinger.com>
2020-09-23 20:34:23 +0200

    feat: Add basic DB integration

--- pages/api/todos.js
+++ pages/api/todos.js
@@ -3,14 +3,19 @@ import query from "../../lib/db";
 const todos = async (req, res) => {
   switch (req.method) {
     case "POST":
-      res.statusCode = 201;
-      res.json({ id: 1, title: req.body.title, body: req.body.body });
+      if (!req.body.title || !req.body.body) {
+        res.statusCode = 422;
+        res.end("missing todo title or body");
 
-      const newTodo = await query`insert into todos (title, body)
-values ('First todo title', 'First todo body')
+        return;
+      }
+
+      const [newTodo] = await query`insert into todos (title, body)
+values (${req.body.title}, ${req.body.body})
 returning *;`;
 
-      console.log(newTodo);
+      res.statusCode = 201;
+      res.json({ id: newTodo.id, title: newTodo.title, body: newTodo.body });
 
       return;
     default:
